34e966a5c3b676570f9c15ada4fccf1373448035
SHIVAJEE <130691874+shivajee98@users.noreply.github.com>
2024-05-15 23:25:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def hello_world():
+    # Redirect to index.html
+    return redirect(url_for('index'))
 
+@app.route('/index.html')
+def index():
+    # Render the index.html template
+    return render_template('index.html')
 
 @app.route('/data')
 def data():
     return 'Data endpoint!'
 
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
-
